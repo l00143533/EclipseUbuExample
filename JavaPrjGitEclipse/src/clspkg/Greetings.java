ec8c9e40feacafcbc5929c047b34e38c87764a0f
l00143533 <l00143533@l00143533-virtual-machine>
2021-01-03 03:57:47 +0000

    Demo commit project

---
+++ JavaPrjGitEclipse/src/clspkg/Greetings.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package clspkg;
+
+/**
+ * @author l00143533
+ *
+ */
+public class Greetings {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello student");
+
+	}
+
+}
