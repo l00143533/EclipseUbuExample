812735438e2c481794432c53e89b547a80493bb5
l00143533 <l00143533@l00143533-virtual-machine>
2021-01-03 04:19:43 +0000

    Added second print line

--- JavaPrjGitEclipse/src/clspkg/Greetings.java
+++ JavaPrjGitEclipse/src/clspkg/Greetings.java
@@ -15,6 +15,7 @@ public class Greetings {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hello student");
+		System.out.print("Hello student2");
 
 	}
 
